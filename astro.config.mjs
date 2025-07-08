a12649afc11d86296ed84ca8fe22b141973f2693
Johannes Borchard <johannesborchard@gmail.com>
2025-07-08 20:11:12 +0200

    update sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import react from "@astrojs/react"
-import sanity from "@sanity/astro"
-import { defineConfig } from "astro/config"
+import react from '@astrojs/react'
+import sanity from '@sanity/astro'
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
 	integrations: [
 		sanity({
-			projectId: "utl6w4lh", // Aus der .env Datei
-			dataset: "production", // Normalerweise "production"
+			projectId: 'utl6w4lh', // Aus der .env Datei
+			dataset: 'production', // Normalerweise "production"
 			useCdn: false,
-			apiVersion: "2025-01-28",
-			studioBasePath: "/studio", // Studio läuft auf /studio
+			apiVersion: '2025-01-28',
+			studioBasePath: '/admin',
 		}),
 		react(),
 	],
