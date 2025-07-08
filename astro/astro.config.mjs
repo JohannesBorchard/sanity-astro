59792f83d27390838c7f95f2e2d5ec5eebf8b6fc
Johannes Borchard <johannesborchard@gmail.com>
2025-07-08 23:02:03 +0200

    init tailwind + prepare shadcn

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,7 +1,23 @@
 import react from '@astrojs/react'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
 	output: 'static',
 	integrations: [react()],
+	vite: {
+		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				'@': '/src',
+				'~': '/src',
+				'~/app': '/src/app',
+				'~/pages': '/src/pages',
+				'~/widgets': '/src/widgets',
+				'~/features': '/src/features',
+				'~/entities': '/src/entities',
+				'~/shared': '/src/shared',
+			},
+		},
+	},
 })
