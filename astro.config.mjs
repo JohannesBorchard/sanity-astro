2408258d8313d1eb37da44b656fd6a766626d1c7
Johannes Borchard <johannesborchard@gmail.com>
2025-07-08 20:50:44 +0200

    astro config improvement

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import sanity from '@sanity/astro'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
+	output: 'static',
 	integrations: [
 		sanity({
 			projectId: 'utl6w4lh', // Aus der .env Datei
 			dataset: 'production', // Normalerweise "production"
-			useCdn: false,
+			useCdn: process.env.NODE_ENV === 'production',
 			apiVersion: '2025-01-28',
 			studioBasePath: '/admin',
 		}),
